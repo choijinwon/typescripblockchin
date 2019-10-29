bacba73547dd6e99f2ff09c1d9a9a206ca141852
최진원 <choejin-won@choejin-won-ui-MacBookPro.local>
2019-10-29 17:48:02 +0900

    block fix

--- src/index.ts
+++ src/index.ts
@@ -37,5 +37,30 @@ const genesisBlock:Block = new Block(0,"1111111111","","Lancoin",123456);
 let blockchin:Block[] = [genesisBlock];
 const getBlockchain = () : Block[] => blockchin;
 const getLatestBlock= () : Block => blockchin[blockchin.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() /1000)
 
+
+const createNewBlock = (data:string) : Block => {
+   const previosBlock : Block = getLatestBlock();
+   const newIndex:number = previosBlock.index++;
+   const newTimestamp: number =getNewTimeStamp();
+   const newHash :string =Block.calculateBlockHash
+   (
+      newIndex,
+      previosBlock.hash,
+      newTimestamp,
+      data
+   );
+
+   const newBlock : Block = new Block(
+      newIndex,
+      newHash,
+      previosBlock.hash,
+      data,
+      newTimestamp
+   );
+   return newBlock;
+};
+
+console.log(createNewBlock("Hello"),createNewBlock("bye bye"));
 export{};
