6be92936ca8c7d43d5af975adf8fdd96803b4a8e
최진원 <choejin-won@choejin-won-ui-MacBookPro.local>
2019-10-29 17:35:11 +0900

    function Create new block

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
    public index:number;
    public hash:string;
    public previousHash:string;
    public data:string;
    public timestamp:number;
+
+   static calculateBlockHash =
+    (
+      index:number,
+      previousHash:string, 
+      timestamp:number,
+      data:string
+   ): string => CryptoJS.SHA256(index+previousHash+timestamp+data).toString();
+
    constructor(
       index:number,
       hash:string,
@@ -21,8 +32,10 @@ class Block {
 }
 
 
+
 const genesisBlock:Block = new Block(0,"1111111111","","Lancoin",123456);
-let blockchin:[Block] = [genesisBlock];
-console.log(blockchin);
+let blockchin:Block[] = [genesisBlock];
+const getBlockchain = () : Block[] => blockchin;
+const getLatestBlock= () : Block => blockchin[blockchin.length - 1];
 
 export{};
